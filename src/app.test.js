61297ed2822131cafc8fa02fb8eb1e355dac0e48
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-07-02 15:50:05 -0400

    fix merge conflicts

--- src/app.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { App } from './App'
-
-test('renders NeuroBridge brand', () => {
-  render(<App />)
-  const brandElement = screen.getByRole('banner')
-  expect(brandElement).toBeInTheDocument()
-})
