48d99da2d37897fedbd1ab3a35c0df0c69904005
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-07-02 13:43:50 -0400

    terms array now displaying in the console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Container, Header, Grid, Form, Input, Button } from "semantic-ui-react"
+import { Container, Header, Grid, Input, Loader, Dimmer } from "semantic-ui-react"
 import './App.css';
 import axios from 'axios'
 
@@ -27,9 +27,9 @@ function App() {
 
     const fetchTerms = async () => {
       try {
-        const { data } = await axios.get(`http://www.ebi.ac.uk/ols/api/ontologies/${searchVal}/terms`)
+        const { data } = await axios.get(`https://www.ebi.ac.uk/ols/api/ontologies/${searchVal}/terms`)
         if(!data) {
-          throw new Error('an error occured while trying to get the results')
+          throw new Error('an error occurred while trying to get the results')
         }
         console.log(data)
       } catch (err) {
@@ -45,40 +45,38 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header inverted content="OLS terms" />
+        <Header inverted content="OLS Terms" />
       </header>
       <Container>
         <Grid centered>
           <Grid.Row>
-            <Form>
-              <Form.Field 
-                  id="search-field"
-                  control={Input}
-                  label="Enter an ontology id (e.g. Cardiovascular Disease = cdvo"
-                  placeholder="type an ontology id here"
-                  value={searchVal}
-                  onKeyDown={handleKeyDown}
-                  onChange={handleInputChange}
+            <Grid.Column>
+              <p style={{marginTop: "2rem"}}><b>Enter an ontology id/short name</b></p>
+              <Input 
+                id="search-field"
+                action={{icon: "search", onClick: handleSubmit}}
+                size="large"
+                placeholder="e.g. Cardiovascular Disease = cvdo"
+                value={searchVal}
+                onKeyDown={handleKeyDown}
+                onChange={handleInputChange}
+                fluid
               />
-              <Form.Field 
-                  id="submit"
-                  control={Button}
-                  content="Search"
-                  onClick={handleSubmit}
-              />
-            </Form>
+            </Grid.Column>
           </Grid.Row>
 
           <Grid.Row>
             <Grid.Column>
-              <p className="App-intro">Here are all the terms associated with the ontology you searched for</p>
               <div className="results">
                 {loading && !errMsg ? (
-                  <span>Loading ...</span>
+                  <Dimmer active inverted>
+                    <Loader inverted>Waiting for Results...</Loader>
+                  </Dimmer>
                 ) : errMsg ? (
                   <div className="errorMessage">{errMsg}</div>
-                ) : (
-                  console.log(results)
+                  ) : (
+                  <p className="App-intro">Here are all the terms associated with the ontology you searched for:</p>
+
                 )
               }
               </div>
