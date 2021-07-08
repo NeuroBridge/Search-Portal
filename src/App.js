e814e391031b51762b2dee79a789fd0f346a3738
Laurie Bernadel <55070235+lbernadel@users.noreply.github.com>
2021-07-08 19:08:09 -0400

    rename file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { useState } from "react"
-import { Container, Header, Grid, Input, Loader, Dimmer, List } from "semantic-ui-react"
+import { Input, PageHeader, Row, Col, Spin, List } from "antd"
 import './App.css';
 import axios from 'axios'
 
 import Results from "./components/Results";
 
+const { Search } = Input
+
 function App() {
   const [searchVal, setSearchVal] = useState("")
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState()
   const [results, setResults] = useState([])
   const [errMsg, setErrMsg] = useState(null)
 
-  const handleKeyDown = event => {
-    if (event.keyCode === 13) {
-      handleSubmit()
-    }
-  }
+  // const handleKeyDown = event => {
+  //   if (event.keyCode === 13) {
+  //     handleSubmit()
+  //   }
+  // }
   const handleInputChange = e => {
     setSearchVal(e.target.value)
   }
@@ -47,51 +49,36 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Header inverted content="OLS Terms" />
-      </header>
-      <Container>
-        <Grid centered>
-          <Grid.Row>
-            <Grid.Column>
-              <p style={{marginTop: "2rem"}}><b>Enter an ontology id/short name</b> (<a href="https://www.ebi.ac.uk/ols/ontologies" target="_blank" rel="noreferrer">List of all ontologies</a>)</p>
-              <Input 
-                id="search-field"
-                action={{icon: "search", onClick: handleSubmit}}
-                size="large"
-                placeholder="e.g. Chemical Methods = chmo"
-                value={searchVal}
-                onKeyDown={handleKeyDown}
-                onChange={handleInputChange}
-                fluid
-              />
-            </Grid.Column>
-          </Grid.Row>
+      <PageHeader title="OLS Terms" ghost={false} />
+      <Row>
+        <Col span={12} offset={6}>
+          <p style={{marginTop: "2rem"}}><b>Enter an ontology id/short name</b> (<a href="https://www.ebi.ac.uk/ols/ontologies" target="_blank" rel="noreferrer">List of all ontologies</a>)</p>
+          <Search id="search-field" placeholder="e.g. Chemical Methods = chmo" onSearch={handleSubmit} onChange={handleInputChange} value={searchVal} allowClear enterButton />
+        </Col>
+      </Row>
 
-          <Grid.Row>
-            <Grid.Column>
-              <div className="results">
-                {loading && !errMsg ? (
-                  <Dimmer active inverted>
-                    <Loader inverted>Waiting for Results...</Loader>
-                  </Dimmer>
-                ) : errMsg ? (
-                  <div className="errorMessage">{errMsg}</div>
-                  ) : (
-                    //console.log("this is results", results)
-                    <span>
-                      <Header size="medium" className="App-intro">Here are all the terms associated with the <b>{searchVal}</b> ontology:</Header>
-                      <List className="results">
-                        {results.map(result => <Results term={result} />)}
-                      </List>
-                    </span>
-                )
-              }
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+      <Row>
+        <Col span={12} offset={6}>
+          <div className="results">
+            {loading && !errMsg ? (
+                <Spin tip="Getting Results..." />
+            ) : errMsg ? (
+              <div className="errorMessage">{errMsg}</div>
+            ) : !loading && searchVal === "" ? (
+                <Spin tip="Enter a search term..." />
+            ) : (
+              //console.log("this is results", results)
+              <span>
+                <p className="App-intro">Here are all the terms associated with the <b>{searchVal}</b> ontology:</p>
+                <List className="results">
+                  {results.map(result => <Results term={result} />)}
+                </List>
+              </span>
+            )
+          }
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
