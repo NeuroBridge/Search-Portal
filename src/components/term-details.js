4a155ddf28f728a081effcd6bf8b9daa0a9afc4d
Matt Watson <mattwat@gmail.com>
2021-08-21 20:42:02 -0400

    tweak json styling

--- src/components/term-details.js
+++ src/components/term-details.js
@@ -1,6 +1,6 @@
 export const TermDetails = ({ term }) => {
   return (
-    <pre style={{ fontSize: '80%' }}>
+    <pre style={{ fontSize: '80%', padding: '1rem', overflow: 'hidden' }}>
       { JSON.stringify(term, null, 2) }
     </pre>
   )
