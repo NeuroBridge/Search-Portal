456f35a3cfadf21138ace9e937a5096fe9500d68
Matt Watson <mattwat@gmail.com>
2021-09-01 16:15:47 -0400

    remove debug logging

--- src/api.js
+++ src/api.js
@@ -14,7 +14,6 @@ export const api = {
       if (!data) {
         throw new Error('An error occurred while fetching terms')
       }
-      console.log(data.response)
       return data.response.docs
     } catch (error) {
       console.log(error)
