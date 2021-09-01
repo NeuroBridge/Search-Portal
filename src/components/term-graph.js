a4192a2cf2316344c92211bb85726415957e5a6c
Matt Watson <mattwat@gmail.com>
2021-09-01 13:43:49 -0400

    fetch and log children on node click
    cleanup, add style

--- src/components/term-graph.js
+++ src/components/term-graph.js
@@ -19,6 +19,19 @@ const useStyles = makeStyles(theme => ({
     minHeight: '400px',
     width: '100%',
   },
+  tooltip: {
+    padding: theme.spacing(1),
+    backgroundColor: 'grey',
+    border: '1px solid #333',
+  },
+  tooltipTitle: {
+    margin: 0,
+    fontSize: '90%',
+  },
+  tooltipDetail: {
+    margin: 0,
+    fontSize: '65%',
+  }
 }))
 
 export const TermGraph = ({ term }) => {
@@ -36,9 +49,9 @@ export const TermGraph = ({ term }) => {
           const [children, parents] = responses
           const parent = parents[0]
           const nodes = [
-            { id: parent.short_form, name: parent.short_form, val: 10, color: 'slategrey' },
-            { id: term.short_form, name: term.short_form, val: 15, color: 'salmon' },
-            ...children.map(child => ({ id: child.short_form, name: child.short_form, val: 10, color: 'slategrey' })),
+            { id: parent.short_form, name: parent.short_form, val: 10, color: 'slategrey', iri: parent.iri },
+            { id: term.short_form, name: term.short_form, val: 15, color: 'indianred', iri: term.iri },
+            ...children.map(child => ({ id: child.short_form, name: child.short_form, val: 10, color: 'lightsalmon', iri: child.iri })),
           ]
           const links = [
             ...parents.map(parent => ({ source: parent.short_form, target: term.short_form })),
@@ -50,6 +63,18 @@ export const TermGraph = ({ term }) => {
         })
         .catch(error => console.log(error))
     }
+  }, [term])
+
+  const tooltip = ({ name, iri, color }) => `
+    <div class="${ classes.tooltip }" style="background-color: ${ color }">
+      <h3 class="${ classes.tooltipTitle }">${ name }</h3>
+      <p class="${ classes.tooltipDetail }">${ iri }</p>
+    </div>`
+
+  const handleNodeClick = useCallback(async (node, event) => {
+    const children = await api.hierarchicalChildren(encodeURIComponent(encodeURIComponent(node.iri)))
+    console.log(`"${ node.id }" has ${ children.length } children:`)
+    console.table(children.map(term => term.short_form))
   }, [term])
 
   return (
@@ -72,6 +97,8 @@ export const TermGraph = ({ term }) => {
             linkDirectionalParticles={ 2 }
             linkDirectionalParticleWidth={ 2 }
             d3VelocityDecay={ 0.5 }
+            onNodeClick={ (node, event) => handleNodeClick(node, event) }
+            nodeLabel={ node => tooltip({ ...node }) }
           />
         )
       }
