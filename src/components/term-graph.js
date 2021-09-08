63d5633d3d264a7dbd542ef43b0f36c761053f20
Matt Watson <mattwat@gmail.com>
2021-09-08 10:34:40 -0400

    get colors straight, indicate selection

--- src/components/term-graph.js
+++ src/components/term-graph.js
@@ -1,23 +1,24 @@
 import { forwardRef, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import axios from 'axios'
-import {
-  Paper
-} from '@material-ui/core'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { Paper, Typography } from '@material-ui/core'
 import {
   ChevronLeft as PreviousTermIcon,
   ChevronRight as NextTermIcon,
 } from '@material-ui/icons'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import { api } from '../api'
 import ReactJson from 'react-json-view'
 import ForceGraph2D from 'react-force-graph-2d'
 import { useSearchContext } from '../context'
 import { SizeMe } from 'react-sizeme'
 
+const SELECTED_NODE_COLOR = 'crimson'
+
 const useStyles = makeStyles(theme => ({
   root: {
     minHeight: '91%',
     width: '100%',
+    position: 'relative',
     '& > div': {
       width: '100%',
       backgroundColor: 'var(--color-unc-gray)'
@@ -35,19 +36,25 @@ const useStyles = makeStyles(theme => ({
   tooltipDetail: {
     margin: 0,
     fontSize: '65%',
+  },
+  selectionNote: {
+    position: 'absolute',
+    bottom: '1rem',
+    left: '1rem',
   }
 }))
 
 export const TermGraph = ({ term, height, width }) => {
   const classes = useStyles()
   const [graphData, setGraphData] = useState({ nodes: [], links: [] })
-  const [highlightedNodes, setHighlightedNodes] = useState([])
+  const [selectedNodes, setSelectedNodes] = useState([])
   const container = useRef()
 
   const visibleNodes = useMemo(() => graphData ? graphData.nodes.map(node => node.id) : [], [graphData.nodes])
 
   useEffect(() => {
     if (term) {
+      setSelectedNodes([])
       const promises = [
         api.hierarchicalChildren(encodeURIComponent(encodeURIComponent(term.iri))),
         api.hierarchicalParents(encodeURIComponent(encodeURIComponent(term.iri))),
@@ -57,16 +64,15 @@ export const TermGraph = ({ term, height, width }) => {
           const [children, parents] = responses
           const parent = parents[0]
           const nodes = [
-            { id: parent.short_form, name: parent.short_form, val: 15, color: 'maroon', iri: parent.iri, hasChildren: parent.has_children },
-            { id: term.short_form, name: term.short_form, val: 15, color: 'orangered', iri: term.iri, hasChildren: term.has_children },
-            ...children.map(child => ({ id: child.short_form, name: child.short_form, val: 15, color: 'indianred', iri: child.iri, hasChildren: child.has_children })),
+            { id: parent.short_form, name: parent.short_form, val: 15, color: 'indianred', iri: parent.iri, hasChildren: parent.has_children }, // add parent term node
+            { id: term.short_form, name: term.short_form, val: 15, color: 'coral', iri: term.iri, hasChildren: term.has_children }, // add current term node
+            ...children.map(child => ({ id: child.short_form, name: child.short_form, val: 15, color: 'indianred', iri: child.iri, hasChildren: child.has_children })), // add current term's children nodes
           ]
           const links = [
-            ...parents.map(parent => ({ source: parent.short_form, target: term.short_form })),
-            ...children.map(child => ({ source: term.short_form, target: child.short_form })),
+            ...parents.map(parent => ({ source: parent.short_form, target: term.short_form })), // parent-current term edge
+            ...children.map(child => ({ source: term.short_form, target: child.short_form })), // children-current term edge
           ]
           setGraphData({ nodes, links })
-          setHighlightedNodes([term.short_form, parent.short_form, ...children.map(child => child.short_form)])
         }).catch(error => console.log(error))
     }
   }, [term])
@@ -92,18 +98,38 @@ export const TermGraph = ({ term, height, width }) => {
     })
   }
 
-  console.log(container)
+  const handleNodeRightClick = (node, event) => {
+    const newSelection = new Set(selectedNodes)
+    if (newSelection.has(node.id)) {
+      console.log(`deselecting`, node.id)
+      newSelection.delete(node.id)
+    } else {
+      console.log(`selecting`, node.id)
+      newSelection.add(node.id)
+    }
+    setSelectedNodes(Array.from(newSelection))
+  }
 
-  const nodePaint = ({ id, x, y, hasChildren }, color, ctx) => {
-    if (hasChildren) {
+  const nodePaint = ({ id, x, y, color, hasChildren }, ctx) => {
+    if (selectedNodes.includes(id)) {
       ctx.beginPath()
-      ctx.strokeStyle = color
+      ctx.strokeStyle = SELECTED_NODE_COLOR
       ctx.fillStyle = 'transparent'
-      ctx.lineWidth = 0.25
-      ctx.arc(x, y, 5, 0, 2 * Math.PI, false)
+      ctx.lineWidth = 1
+      ctx.arc(x, y, 6, 0, 2 * Math.PI, false)
       ctx.stroke()
       ctx.fill()
     }
+    ctx.beginPath()
+    ctx.fillStyle = hasChildren ? color : '#eee'
+    ctx.strokeStyle = hasChildren ? '#eee' : color
+    ctx.lineWidth = hasChildren ? 0.25 : 1
+    ctx.arc(x, y, 4, 0, 2 * Math.PI, false)
+    ctx.stroke()
+    ctx.fill()
+  }
+
+  const nodePointerAreaPaint = ({ id, x, y, hasChildren }, color, ctx) => {
     ctx.beginPath()
     ctx.fillStyle = color
     ctx.arc(x, y, 4, 0, 2 * Math.PI, false)
@@ -129,13 +155,14 @@ export const TermGraph = ({ term, height, width }) => {
               linkDirectionalParticleWidth={ 2 }
               d3VelocityDecay={ 0.5 }
               onNodeClick={ (node, event) => handleNodeClick(node, event) }
+              onNodeRightClick={ handleNodeRightClick }
               nodeLabel={ node => tooltip({ ...node }) }
-              nodeCanvasObject={ (node, ctx) => nodePaint(node, node.color, ctx) }
-              nodePointerAreaPaint={ nodePaint }
+              nodeCanvasObject={ nodePaint }
             />
           )
         }
       </SizeMe>
+      <Typography className={ classes.selectionNote }>{ selectedNodes.length } nodes selected</Typography>
     </Paper>
   )
 }
