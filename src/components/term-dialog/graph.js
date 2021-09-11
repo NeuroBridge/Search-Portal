df49ec0b629069abbe915d0d1ea2786437d94efd
Matt Watson <mattwat@gmail.com>
2021-09-11 17:53:40 -0400

    swap left and right click behaviors

--- src/components/term-dialog/graph.js
+++ src/components/term-dialog/graph.js
@@ -118,7 +118,7 @@ export const TermGraph = ({ term, height, width }) => {
       <p class="${ classes.tooltipDetail }">${ hasChildren ? 'Has children' : 'No children' }</p>
     </div>`
 
-  const handleNodeClick = async (node, event) => {
+  const handleNodeRightClick = async (node, event) => {
     const children = await api.hierarchicalChildren(encodeURIComponent(encodeURIComponent(node.iri)))
     console.log(`"${ node.id }" has ${ children.length } children:`)
     console.table(children.map(child => child.short_form))
@@ -132,7 +132,7 @@ export const TermGraph = ({ term, height, width }) => {
     })
   }
 
-  const handleNodeRightClick = (node, event) => toggleNodeSelection(node.id)
+  const handleNodeLeftClick = (node, event) => toggleNodeSelection(node.id)
 
   const nodePaint = ({ id, x, y, color, hasChildren }, ctx) => {
     if (selectedNodes.includes(id)) {
@@ -178,8 +178,8 @@ export const TermGraph = ({ term, height, width }) => {
               linkDirectionalParticles={ 2 }
               linkDirectionalParticleWidth={ 2 }
               d3VelocityDecay={ 0.5 }
-              onNodeClick={ (node, event) => handleNodeClick(node, event) }
-              onNodeRightClick={ handleNodeRightClick }
+              onNodeClick={ handleNodeLeftClick }
+              onNodeRightClick={ (node, event) => handleNodeRightClick(node, event) }
               nodeLabel={ node => tooltip({ ...node }) }
               nodeCanvasObject={ nodePaint }
             />
