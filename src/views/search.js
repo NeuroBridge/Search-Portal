9481f14ba7f400d4084e8e2998176c51b9fd4a89
Matt Watson <mattwat@gmail.com>
2021-09-18 19:45:54 -0400

    dump json to search-specific debug view

--- src/views/search.js
+++ src/views/search.js
@@ -1,9 +1,11 @@
-import { Fragment, useCallback, useMemo } from 'react'
+import { Fragment, useCallback, useMemo, useState } from 'react'
 import { useSearchContext } from '../components/search/context'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, IconButton, Paper, Typography } from '@material-ui/core'
 import { TermCard } from '../components/search/term-card'
 import { TermDialog } from '../components/search/term-dialog'
+import { BugReport as DebugIcon } from '@material-ui/icons';
+import ReactJson from 'react-json-view'
 
 const useStyles = makeStyles(theme => ({
   grid: {
@@ -11,12 +13,26 @@ const useStyles = makeStyles(theme => ({
     gridTemplateColumns: 'repeat(auto-fit, minmax(500px, 1fr))',
     gap: theme.spacing(2),
   },
+  resultsHeader: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  debug: {
+    '& .react-json-view': {
+      fontSize: '80%',
+      borderRadius: theme.spacing(1) / 2,
+      padding: theme.spacing(2),
+    }
+  },
 }))
 
 export const SearchView = () => {
   const classes = useStyles()
   const { busy, doSearch, terms, currentTerm, setCurrentTerm, previousTerm, nextTerm, searchedQuery } = useSearchContext()
   const dialogOpen = useMemo(() => !!currentTerm, [currentTerm])
+  const [debugMode, setDebugMode] = useState(false)
 
   const handleClickTerm = index => event => {
     if (0 <= index && index < terms.length) {
@@ -24,20 +40,39 @@ export const SearchView = () => {
     }
   }
 
-  const MemoizedResultsHeader = useCallback(() => <Typography>"{ searchedQuery }" returned { terms.length } results</Typography>, [searchedQuery, terms])
+  const handleToggleDebugMode = () => setDebugMode(!debugMode)
+
+  const MemoizedResultsHeader = useCallback(() => {
+    return (
+      <Paper className={ classes.resultsHeader } elevation={ 0 }>
+        <Typography>"{ searchedQuery }" returned { terms.length } results</Typography>
+        <IconButton onClick={ handleToggleDebugMode }>
+          <DebugIcon color={ debugMode === true ? 'secondary' : 'action' } />
+        </IconButton>
+      </Paper>
+    )
+  }, [debugMode, searchedQuery, terms])
 
   return (
     <Fragment>
       { searchedQuery && <MemoizedResultsHeader /> }
       <br />
-      <Grid container spacing={ 3 }>
-        <Grid item xs={ 12 } className={ classes.grid }>
-          {
-            !!terms.length && terms.map((term, index) => <TermCard term={ term } key={ term.label } clickHandler={ handleClickTerm(index) }/>)
-          }
-          <TermDialog open={ dialogOpen } term={ currentTerm } closeHandler={ handleClickTerm(null) } />
-        </Grid>
-      </Grid>
+      {
+        debugMode ? (
+          <div className={ classes.debug }>
+            <ReactJson src={ terms } theme="monokai" />
+          </div>
+        ) : (
+          <Grid container spacing={ 3 }>
+            <Grid item xs={ 12 } className={ classes.grid }>
+              {
+                !!terms.length && terms.map((term, index) => <TermCard term={ term } key={ term.label } clickHandler={ handleClickTerm(index) }/>)
+              }
+              <TermDialog open={ dialogOpen } term={ currentTerm } closeHandler={ handleClickTerm(null) } />
+            </Grid>
+          </Grid>
+        )
+      }
     </Fragment>
   )
 }
