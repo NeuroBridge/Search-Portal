32029549fb7a145bb11a65509340afae8c188d16
Matt Watson <mattwat@gmail.com>
2021-09-18 20:58:39 -0400

    add search landing view

--- src/views/search.js
+++ src/views/search.js
@@ -40,6 +40,32 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
+const SearchLanding = () => {
+  return (
+    <Fragment>
+      <Typography variant="h4" align="center">Search the NeuroBridge Ontology</Typography>
+      
+      <br />
+      <br />
+
+      <Typography paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Typography>
+
+      <Typography paragraph>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Typography>
+    </Fragment>
+  )
+}
+
 export const SearchView = () => {
   const classes = useStyles()
   const { busy, doSearch, terms, currentTerm, setCurrentTerm, previousTerm, nextTerm, searchedQuery } = useSearchContext()
@@ -69,9 +95,15 @@ export const SearchView = () => {
     )
   }, [debugMode, searchedQuery, terms])
 
+  if (!searchedQuery) {
+    return (
+      <SearchLanding />
+    )
+  }
+
   return (
     <Fragment>
-      { searchedQuery && <MemoizedResultsHeader /> }
+      <MemoizedResultsHeader />
       <br />
       {
         debugMode ? (
