7aae55ce0321f6fec8b6f93529979b30d7a157d4
Matt Watson <mattwat@gmail.com>
2021-09-22 14:36:16 -0400

    add slight opacity to dialog tray

--- src/components/search/term-dialog/tray.js
+++ src/components/search/term-dialog/tray.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles(theme => ({
     height: '100%',
     display: 'flex',
     flexDirection: 'column',
+    opacity: 0.9,
   },
   header: {
     backgroundColor: theme.palette.grey[400],
