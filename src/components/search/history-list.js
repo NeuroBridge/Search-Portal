bff24557c4aa7f6bbb3db4e55b5de087af12df9b
Matt Watson <mattwat@gmail.com>
2021-09-24 13:43:26 -0400

    make history items clickable

--- src/components/search/history-list.js
+++ src/components/search/history-list.js
@@ -1,18 +1,23 @@
-import { Card, CardContent, CardHeader, List, ListItem, ListItemText } from '@material-ui/core'
+import { Card, CardActions, CardContent, CardHeader, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
 import { useLocalStorage } from '../../hooks'
 import { Link } from '../link'
 import TimeAgo from 'react-timeago'
+import { useSearchContext } from './context'
+import { Search as SearchIcon } from '@material-ui/icons'
 
 export const SearchHistoryList = () => {
   const [searchHistory, ] = useLocalStorage('search-history')
+  const { doSearch } = useSearchContext()
 
   return (
     <Card>
       <CardHeader title="Recent Searches" />
+
       <List>
         {
           searchHistory && searchHistory.map(({ query, timestamp }) => (
-            <ListItem key={ `history-${ query }-${ timestamp }`}>
+            <ListItem key={ `history-${ query }-${ timestamp }`} button onClick={ () => doSearch(query) }>
+              <ListItemIcon><SearchIcon /></ListItemIcon>
               <ListItemText
                 primary={ query }
                 secondary={ <TimeAgo date={ timestamp } /> }
@@ -21,6 +26,9 @@ export const SearchHistoryList = () => {
           ))
         }
       </List>
+
+      <CardActions />
+
     </Card>
 
   )
