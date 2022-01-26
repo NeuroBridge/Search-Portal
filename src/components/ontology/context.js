9a79624c2f869b5df08b547da08ff7422d0f36cc
Matt Watson <mattwat@gmail.com>
2022-01-26 18:01:26 -0500

    move all-term-fetching func to ontology context

--- src/components/ontology/context.js
+++ src/components/ontology/context.js
@@ -1,15 +1,40 @@
-import { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import { createContext, useCallback, useContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { api } from '../../api'
+import axios from 'axios'
+
+//
+// See OLS API Documentation: https://www.ebi.ac.uk/ols/docs/api
+//
+
+const API_ROOT = `https://neurobridges.renci.org:8444/api`
+const ONTOLOGY_NAME = 'neurobridges_ontology'
 
 const OntologyContext = createContext({})
 
 export const OntologyProvider = ({ children }) => {
   const [ontology, setOntology] = useState([])
+  
+  const fetchAllTerms = useCallback(async () => {
+    let terms = []
+    const termsEndpoint = `${ API_ROOT }/ontologies/${ ONTOLOGY_NAME }/terms`
+    try {
+      const { data } = await axios.get(termsEndpoint)
+      if (!data) {
+        throw new Error('An error occurred while fetching terms.')
+      }
+      let { page: { totalPages } } = data
+      const promises = [...Array(totalPages).keys()].map(i => axios.get(termsEndpoint, { params: { page: i } }))
+      const responses = await Promise.all(promises)
+      terms = responses.reduce((arr, response) => [...arr, ...response.data._embedded.terms], [])
+    } catch (error) {
+      console.error(error)
+    }
+    return terms
+  }, [])
 
   useEffect(async () => {
     try {
-      let terms = await api.allTerms()
+      let terms = await fetchAllTerms()
       terms = terms.map(term => ({
           ...term,
           id: term.short_form,
