f07693897adafe17cca5fd77aa8be0edd7388c8f
Matt Watson <mattwat@gmail.com>
2022-02-01 11:40:46 -0500

    rethink workspace

---
+++ src/views/about.js
@@ -0,0 +1,32 @@
+import { Fragment } from 'react'
+import { Container } from '../components/container'
+import { PageHeader } from '../components/page-header'
+import { Card, CardContent, CardHeader } from '@mui/material'
+
+export const AboutView = () => {
+  return (
+    <Fragment>
+      <PageHeader title="About NeuroBridge" />
+
+      <Container>
+        <Card sx={{ height: '300px' }}>
+          <CardContent>
+            the quick brown fox junmps over a lazy dog.
+          </CardContent>
+        </Card>
+        <br /><br />
+        <Card sx={{ height: '300px' }}>
+          <CardContent>
+            the quick brown fox junmps over a lazy dog.
+          </CardContent>
+        </Card>
+        <br /><br />
+        <Card sx={{ height: '300px' }}>
+          <CardContent>
+            the quick brown fox junmps over a lazy dog.
+          </CardContent>
+        </Card>
+      </Container>
+    </Fragment>
+  )
+}
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,4 @@
+export * from './about'
 export * from './not-found'
 export * from './workspace'
 export * from './results'
