499a951866a32824a28166ef780f3dfd56418280
Matt Watson <mwatson@renci.org>
2022-05-10 14:28:50 -0400

    hide query workspace if no terms are selected

--- src/components/workspace/workspace.js
+++ src/components/workspace/workspace.js
@@ -1,10 +1,12 @@
 import { createElement, Fragment, useState } from 'react'
-import { Box, Card, CardContent, CardHeader, CircularProgress, Divider, Tab, Tabs } from '@mui/material'
+import { Box, Card, CardContent, CardHeader, CircularProgress, Divider, Fade, Tab, Tabs } from '@mui/material'
 import { services } from './services'
+import { useBasket } from '../basket'
 
 //
 
 export const Workspace = () => {
+  const basket = useBasket()
   const [currentServiceIndex, setCurrentServiceIndex] = useState(0)
   const [results, setResults] = useState([])
   const [loading, setLoading] = useState(false)
@@ -14,55 +16,57 @@ export const Workspace = () => {
   }
 
   return (
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '1rem',
-    }}>
-      <Card sx={{
+    <Fade in={ basket.contents.length > 0 }>
+      <Box sx={{
         display: 'flex',
         flexDirection: 'column',
-        backgroundSize: '1rem 1rem',
-        overflow: 'hidden',
+        gap: '1rem',
       }}>
-        <CardHeader title="Query Workspace" />
-        <CardContent>
-          Select a service
-        </CardContent>
+        <Card sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundSize: '1rem 1rem',
+          overflow: 'hidden',
+        }}>
+          <CardHeader title="Query Workspace" />
+          <CardContent>
+            Select a service
+          </CardContent>
 
-        <Divider />
+          <Divider />
 
-        <CardContent>
-          <Tabs value={ currentServiceIndex } onChange={ handleChangeService }>
-            {
-              services.map(service => (
-                <Tab key={ service.name } label={ service.name } />
-              ))
-            }
-          </Tabs>
-        </CardContent>
+          <CardContent>
+            <Tabs value={ currentServiceIndex } onChange={ handleChangeService }>
+              {
+                services.map(service => (
+                  <Tab key={ service.name } label={ service.name } />
+                ))
+              }
+            </Tabs>
+          </CardContent>
 
-        <CardContent>
-          <Box>
-            { createElement(services[currentServiceIndex].module, { setLoading, setResults }) }
-          </Box>
-        </CardContent>
-      </Card>
+          <CardContent>
+            <Box>
+              { createElement(services[currentServiceIndex].module, { setLoading, setResults }) }
+            </Box>
+          </CardContent>
+        </Card>
 
-      { loading && <CircularProgress /> }
+        { loading && <CircularProgress /> }
 
-      {
-        !loading && results.map((result, i) => (
-          <Card key={ `${ i }_${ result.pmid }` } >
-            <CardContent>
-              <pre>
-                { JSON.stringify(result, null, 2) }
-              </pre>
-            </CardContent>
-          </Card>
-        ))
-      }
-    </Box>
+        {
+          !loading && results.map((result, i) => (
+            <Card key={ `${ i }_${ result.pmid }` } >
+              <CardContent>
+                <pre>
+                  { JSON.stringify(result, null, 2) }
+                </pre>
+              </CardContent>
+            </Card>
+          ))
+        }
+      </Box>
+    </Fade>
   )
 }
 
