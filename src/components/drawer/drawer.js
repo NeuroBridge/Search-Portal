7e5a7b078e7efa67150ec586ca8e9e927a3b372d
Matt Watson <mwatson@renci.org>
2022-05-10 16:04:11 -0400

    replace parent button for drawer navigation

--- src/components/drawer/drawer.js
+++ src/components/drawer/drawer.js
@@ -53,14 +53,18 @@ export const Drawer = () => {
             gap: '0.5rem',
             padding: '0.5rem 2.4rem',
           }}>
-            <Typography>
-              { drawer.currentTerm.parentId || 'No parent' }
-            </Typography>
-            {
+          <Button
+            variant="text"
+            onClick={ () => drawer.setTermId(drawer.currentTerm.parentId) }
+            disabled={ drawer.currentTerm.parentId === null }
+          >
+            { drawer.currentTerm.parentId || 'No parent' }
+          </Button>            {
               drawer.currentTerm.parentId && (
                 <TermActionButtons
                   termId={ drawer.currentTerm.parentId }
                   tooltipPlacement="top"
+                  hideDrawerButton
                 />
               )
             }
