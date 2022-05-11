1ba427c82fa4e54bc3802b14e9a9f1fd2446682e
Matt Watson <mwatson@renci.org>
2022-05-11 18:47:31 -0400

    allow user to switch between term labels before sending request to NQ

--- src/components/workspace/services/neuroquery.js
+++ src/components/workspace/services/neuroquery.js
@@ -1,7 +1,7 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
-import { Box, Button, Divider } from '@mui/material'
+import { Box, Button, Divider, MenuItem, Select } from '@mui/material'
 import { useBasket } from '../../basket'
 import { useOntology } from '../../ontology'
 
@@ -10,16 +10,19 @@ const BASE_URL = `https://neurobridges.renci.org:13374/query`
 export const NeuroQueryServiceInterface = ({ setLoading, setResults }) => {
   const basket = useBasket()
   const ontology = useOntology()
-  
+  const [termLabels, setTermLabels] = useState({})
+
   const terms = useMemo(() => {
     return basket.ids.filter(id => basket.contents[id] === 1)
       .map(item => ontology.find(item))
   }, [basket.ids])
 
-  const querystring = useMemo(() => {
-    return terms.map(term => term.labels[0]).join('+')
+  useEffect(() => {
+    const newTermLabels = terms.reduce((obj, term) => ({ [term.id]: 0, ...obj }), {})
+    setTermLabels({ ...newTermLabels, ...termLabels })
   }, [terms])
-  
+
+  const querystring = useMemo(() => terms.map(term => term.labels[termLabels[term.id]]).join('+'), [termLabels])
   const url = useMemo(() => `${ BASE_URL }?${ querystring }`, [querystring])
 
   const handleClickQueryButton = () => {
@@ -44,11 +47,32 @@ export const NeuroQueryServiceInterface = ({ setLoading, setResults }) => {
     fetchResults()
   }
 
+  const handleChangeTermLabel = id => event => {
+    const newTermLabels = { ...termLabels, [id]: event.target.value }
+    setTermLabels(newTermLabels)
+  }
+
   return (
     <Box>
       <pre>
         terms: { JSON.stringify(terms.map(term => term.labels[0])) }
       </pre>
+      {
+        terms.map(term => (
+          <Select
+            key={ `${ term.id }-select` }
+            id={ `${ term.id }-select` }
+            value={ termLabels[term.id] || 0 }
+            onChange={ handleChangeTermLabel(term.id) }
+          >
+            {
+              term.labels.map((label, i) => (
+                <MenuItem key={ `${ term.id }-label-${ i }` } value={ i }>{ label }</MenuItem>
+              ))
+            }
+          </Select>
+        ))
+      }
       <pre>
         url: { JSON.stringify(url) }
       </pre>
