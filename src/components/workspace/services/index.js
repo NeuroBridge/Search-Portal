1d262375c3f8c8e4c086b7450e4ead3219d47cbc
Matt Watson <mwatson@renci.org>
2022-05-14 05:41:31 -0400

    restructure imports

--- src/components/workspace/services/index.js
+++ src/components/workspace/services/index.js
@@ -1,17 +1,7 @@
-import * as NQ from './neuroquery'
-import * as NB from './neurobridge'
+import * as NeuroQuery from './neuroquery'
+import * as NeuroBridge from './neurobridge'
 
 export const services = [
-  {
-    id: 'nq',
-    name: 'NeuroQuery',
-    Interface: NQ.Interface,
-    HelpText: NQ.HelpText,
-  },
-  {
-    id: 'nb',
-    name: 'NeuroBridge',
-    Interface: NB.Interface,
-    HelpText: NB.HelpText,
-  },
+  { id: 'nq',     name: 'NeuroQuery',     ...NeuroQuery },
+  { id: 'nb',     name: 'NeuroBridge',    ...NeuroBridge },
 ]
