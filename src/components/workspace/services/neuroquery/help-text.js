0dcd8594b69f44ea9282d033900c35ce5927c8d3
Matt Watson <mwatson@renci.org>
2022-05-16 14:49:53 -0400

    rework help texts

--- src/components/workspace/services/neuroquery/help-text.js
+++ src/components/workspace/services/neuroquery/help-text.js
@@ -1,19 +1,21 @@
 import { CardContent, Typography } from '@mui/material'
 import { Link } from '../../../link'
+import { Check as CheckIcon } from '@mui/icons-material'
 
 export const HelpText = () => {
   return (
     <CardContent>
       <Typography paragraph>
         This interface allows communication with <Link to="https://neuroquery.org/">NeuroQuery</Link>,
-        which receives terms and returns PubMed publications.
+        which returns PubMed publications.
       </Typography>
       <Typography paragraph>
-        Terms in your workspace will appear in this interface as selection boxes.
-        Many terms in the NeuroBridge Ontology have multiple string representations, or <em>labels</em>.
-        Before sending your request to NeuroQuery, you may fine-tune your NeuroQuery search by
-        selecting the most appropriate label to represent each term.
-        Verify the constructed URL and query before sending your request to NeuroQuery.
+        Terms checked <CheckIcon sx={{ color: '#6c6' }} fontSize="small" /> in
+        your workspace appear here as selection boxes. Many terms in the
+        NeuroBridge Ontology have multiple string representations, or <em>labels</em>.
+        Before sending your request to NeuroQuery, you may fine-tune your NeuroQuery
+        search by selecting the most appropriate label to represent each term. Verify
+        the constructed URL and query before sending your request to NeuroQuery.
       </Typography>
     </CardContent>
   )
--- src/components/workspace/services/xiaochen/help-text.js
+++ src/components/workspace/services/xiaochen/help-text.js
@@ -1,13 +1,18 @@
 import { CardContent, Typography } from '@mui/material'
+import { Check as CheckIcon } from '@mui/icons-material'
 
 export const HelpText = () => {
   return (
     <CardContent>
       <Typography paragraph>
         This interface allows communication with the NeuroBridge API,
-        which receives terms and returns PubMed publications.
+        which returns PubMed publications.
+        The current iteration of this interface restricts the user to
+        constructing a query of the form <code>termA AND termB AND ...</code>
       </Typography>
       <Typography paragraph>
+        Terms checked <CheckIcon sx={{ color: '#6c6' }} fontSize="small" /> in
+        your workspace appear here as selection boxes. Many terms in the
         This interface is still under active development.
       </Typography>
     </CardContent>
