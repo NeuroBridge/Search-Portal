e4362d3dfd6bc96d7195261c68ab8719c0abf8ff
Matt Watson <mwatson@renci.org>
2022-05-23 11:52:45 -0400

    update query property name

--- src/components/workspace/services/neurobridge/interface.js
+++ src/components/workspace/services/neurobridge/interface.js
@@ -48,12 +48,12 @@ export const Interface = ({ searchWrapper }) => {
   }
 
   const handleClickQueryButton = () => {
-    console.log(`PAYLOAD: ${ JSON.stringify({ expression: query }, null, 2) }`)
+    console.log(`PAYLOAD: ${ JSON.stringify({ query }, null, 2) }`)
     searchWrapper(async () => {
       try {
         const { data } = await axios.post(
           API_URL,
-          JSON.stringify({ expression: query }),
+          JSON.stringify({ query }),
           { headers: { 'content-type': 'text/html;charset=utf-8' } },
         )
         if (!data) {
