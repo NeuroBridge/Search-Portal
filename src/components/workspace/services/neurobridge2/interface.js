c4fdbc70dbc6d379e2d6dfa64551bcd36e2c00f8
Matt Watson <mwatson@renci.org>
2022-05-23 15:42:39 -0400

    query -> expression

--- src/components/workspace/services/neurobridge2/interface.js
+++ src/components/workspace/services/neurobridge2/interface.js
@@ -94,12 +94,12 @@ export const Interface = ({ searchWrapper }) => {
   }, [roots, values])
 
   const fetchResults = () => {
-    console.log(`PAYLOAD: ${ JSON.stringify({ query }, null, 2) }`)
+    console.log(`PAYLOAD: ${ JSON.stringify({ expression: query }, null, 2) }`)
     searchWrapper(async () => {
       try {
         const { data } = await axios.post(
           API_URL,
-          JSON.stringify({ query }),
+          JSON.stringify({ expression: query }),
           { headers: { 'Content-Type': 'text/html;charset=utf-8' } },
         )
         if (!data) {
