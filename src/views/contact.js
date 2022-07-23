5cbabf95805a291e6a7cf541bc0ff3c3c61fc609
Matt Watson <mwatson@renci.org>
2022-07-23 12:30:47 -0400

    create initial contact view

---
+++ src/views/contact.js
@@ -0,0 +1,20 @@
+import { Container, Typography } from '@mui/material'
+
+export const ContactView = () => {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h4" component="h1" align="center">
+        Contact
+      </Typography>
+
+      <br />
+      
+      <Typography paragraph>
+        Cillum aute exercitation sit nostrud ea fugiat irure sint ut dolore esse tempor ullamco culpa adipisicing elit.
+        Esse quis aliquip ut adipisicing nulla magna eu excepteur do sunt reprehenderit deserunt ad sed minim.
+        Velit nisi in occaecat in officia in culpa amet sit sint reprehenderit.
+      </Typography>
+
+    </Container>
+  )
+}
