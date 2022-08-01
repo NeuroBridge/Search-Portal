6199877be721595a06bbc565c9b4eba709498201
Matt Watson <mwatson@renci.org>
2022-08-01 17:16:00 -0400

    reimplement contact form with yup and react-hook-form
    install yup
    
    install react-hook-form
    
    cleanup
    
    render form in a card component
    
    add comment
    
    make email field not required
    
    add subject field
    
    some cleanup

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,4 +1,4 @@
-import { Container, Typography } from '@mui/material'
+import { Card, CardContent, Container, Divider, Typography } from '@mui/material'
 import { ContactForm } from '../components/contact-form'
 
 export const ContactView = () => {
@@ -10,15 +10,23 @@ export const ContactView = () => {
 
       <br />
       
-      <Typography paragraph>
-        Cillum aute exercitation sit nostrud ea fugiat irure sint ut dolore esse tempor ullamco culpa adipisicing elit.
-        Esse quis aliquip ut adipisicing nulla magna eu excepteur do sunt reprehenderit deserunt ad sed minim.
-        Velit nisi in occaecat in officia in culpa amet sit sint reprehenderit.
-      </Typography>
+      <Card>
+        <CardContent>
+          <Typography paragraph>
+            Cillum aute exercitation sit nostrud ea fugiat irure sint ut dolore esse tempor ullamco culpa adipisicing elit.
+            Esse quis aliquip ut adipisicing nulla magna eu excepteur do sunt reprehenderit deserunt ad sed minim.
+            Velit nisi in occaecat in officia in culpa amet sit sint reprehenderit.
+          </Typography>
 
-      <br />
-      
-      <ContactForm />
+          <br />
+
+          <Divider />
+
+          <br /><br />
+          
+          <ContactForm />
+        </CardContent>
+      </Card>
 
     </Container>
   )
