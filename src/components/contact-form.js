6bcadb3e16a4f2694a0558a6e0b4567957bc40c3
Matt Watson <mwatson@renci.org>
2022-08-01 17:16:00 -0400

    reimplement contact form with yup and react-hook-form
    install yup
    
    install react-hook-form
    
    cleanup
    
    render form in a card component
    
    add comment
    
    make email field not required
    
    add subject field
    
    some cleanup

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,73 +1,101 @@
-import { useCallback, useState } from 'react'
-import { Button, Stack, TextField } from '@mui/material'
+import { useEffect } from 'react'
+import { Button, FormControl, FormHelperText, InputLabel, MenuItem, Select, Stack, TextField } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
-const initialformValues = {
-  name: '',
-  email: '',
-  message: '',
-  submitted: false,
-  success: false,
-}
-
-const useForm = () => {
-  const [values, setValues] = useState(initialformValues)
-  const [errors, setErrors] = useState({})
-  
-  const validate = useCallback(() => {
-    // validate form field values
-  }, [])
-
-  const handleChangeValue = useCallback(event => {
-    const { name, value } = event.target
-    setValues({ ...values, [name]: value })
-  }, [values])
-
-  const handleSubmit = useCallback(event => {
-    console.log('submit!')
-    console.table(values)
-  }, [values])
-
-  const isValid = () => {
-    // check validity
-  }
-
-  return {
-    handleChangeValue,
-    handleSubmit,
-    isValid,
-    errors,
-    values,
-  }
-}
+const schema = yup.object().shape({
+  name: yup.string().required('Please enter your name.'),
+  email: yup.string().email('Invalid email format'),
+  subject: yup.string().oneOf(['question', 'other', 'suggestion', 'technical-difficulties']).required('Please select the most applicable subject.'),
+  message: yup.string().required('Please enter a message.'),
+})
 
 export const ContactForm = () => {
-  const { handleChangeValue, handleSubmit, values } = useForm()
+  const {
+    handleSubmit,
+    formState,
+    register,
+  } = useForm({
+    schema,
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = data => {
+    console.log('SUBMIT')
+    console.table(data)
+  }
+
+  // debugging
+  useEffect(() => {
+    if (formState) {
+      console.table(formState.errors)
+    }
+  })
 
   return (
-    <form>
-      <Stack spacing={ 2 }>
-        <TextField
-          name="name"
-          label="Name"
-          fullWidth
-          onChange={ handleChangeValue }
-        />
-        <TextField
-          name="email"
-          label="Email"
-          fullWidth
-          onChange={ handleChangeValue }
-        />
-        <TextField
-          name="message"
-          label="Message"
-          fullWidth
-          multiline rows={ 5 }
-          onChange={ handleChangeValue }
-        />
-        <Button onClick={ handleSubmit } variant="contained">Submit</Button>
-        <pre>{ JSON.stringify(values, null, 2) }</pre>
-      </Stack>
-    </form>
+    <Stack spacing={ 2 }>
+      { /* NAME */ }
+      <TextField
+        name="name"
+        label="Name"
+        variant="outlined"
+        fullWidth
+        { ...register('name') }
+        error={ !!formState.errors.name }
+      />
+      {
+        'name' in formState.errors && <FormHelperText>{ formState.errors.name.message }</FormHelperText>
+      }
+
+      { /* EMAIL */ }
+      <TextField
+        name="email"
+        label="Email"
+        variant="outlined"
+        fullWidth
+        { ...register('email') }
+        error={ !!formState.errors.email }
+      />
+      {
+        'email' in formState.errors && <FormHelperText>{ formState.errors.email.message }</FormHelperText>
+      }
+
+      { /* SUBJECT */ }
+      <FormControl fullWidth>
+        <InputLabel id="subject-select-label">Subject</InputLabel>
+        <Select
+          labelId="subject-select-label"
+          name="subject"
+          label="Subject"
+          variant="outlined"
+          { ...register('subject') }
+          error={ !!formState.errors.subject }
+        >
+          <MenuItem value="question">Question</MenuItem>
+          <MenuItem value="suggestion">Suggestion</MenuItem>
+          <MenuItem value="technical-difficulties">Technical difficulties</MenuItem>
+          <MenuItem value="other">Other</MenuItem>
+        </Select>
+      </FormControl>
+      {
+        'subject' in formState.errors && <FormHelperText>{ formState.errors.subject.message }</FormHelperText>
+      }
+
+      { /* MESSAGE */ }
+      <TextField
+        name="message"
+        label="Message"
+        variant="outlined"
+        fullWidth
+        { ...register('message') }
+        error={ !!formState.errors.message }
+        multiline rows={ 5 }
+      />
+      {
+        'message' in formState.errors && <FormHelperText>{ formState.errors.message.message }</FormHelperText>
+      }
+      <Button onClick={ handleSubmit(onSubmit) } variant="contained">Submit</Button>
+    </Stack>
   )
 }
