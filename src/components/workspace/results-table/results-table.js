9be3cccc926cfe0563aac99f637d4911ed7f9889
Matt Watson <mwatson@renci.org>
2022-09-03 16:34:24 -0400

    add checkbox selection

--- src/components/workspace/results-table/results-table.js
+++ src/components/workspace/results-table/results-table.js
@@ -72,6 +72,7 @@ export const SearchResultsTable = () => {
                 .join(', ')
             },
           }}
+          checkboxSelection
           onRowClick={ handleRowClick }
         />
       </Card>
