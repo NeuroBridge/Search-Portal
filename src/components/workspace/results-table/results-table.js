b22e4036df629f2e9142addfc46ccc772996f112
Matt Watson <mwatson@renci.org>
2022-09-26 23:21:01 -0400

    move dialog code to table

--- src/components/workspace/results-table/results-table.js
+++ src/components/workspace/results-table/results-table.js
@@ -1,12 +1,20 @@
-import { useMemo, useState } from 'react'
-import { Card, Fade } from '@mui/material'
+import { useEffect, useMemo, useState } from 'react'
+import {
+  Card, Dialog, Divider, DialogContent, DialogTitle,
+  Fade, Stack, Typography,
+} from '@mui/material'
 import { useWorkspace } from '../workspace'
 import {
   DataGrid,
 } from '@mui/x-data-grid'
+import { Link } from '../../link'
 import { columns } from './columns'
 import { TableHeader } from './table-header'
-import { PublicationDialog } from './publication-dialog'
+import nihLogoIcon from '../../../images/pubmed-icon.png'
+
+//
+
+const NihLogo  = () => <img src={ nihLogoIcon } height="12" />
 
 //
 
@@ -14,6 +22,7 @@ export const SearchResultsTable = () => {
   const { results } = useWorkspace()
   const [currentTabIndex, setCurrentTabIndex] = useState(0)
   const [activeRow, setActiveRow] = useState()
+  const [dialogOpen, setDialogOpen] = useState(false)
 
   const handleChangeTab = (event, newIndex) => {
     setCurrentTabIndex(newIndex)
@@ -35,11 +44,19 @@ export const SearchResultsTable = () => {
     setActiveRow(params.row)
   }
 
-  //
-
-  if (!Object.keys(results).length) {
-    return <div />
+  const handleCloseDialog = () => {
+    setDialogOpen(false)
+    const unmountDelay = setTimeout(() => setActiveRow(null), 250)
+    return () => clearTimeout(unmountDelay)
   }
+
+  useEffect(() => {
+    if (activeRow) {
+      setDialogOpen(true)
+    }
+  }, [activeRow])
+
+  //
 
   return (
     <Fade in={ !!Object.keys(results).length }>
@@ -65,9 +82,42 @@ export const SearchResultsTable = () => {
           checkboxSelection
           onRowClick={ handleRowClick }
         />
-        <PublicationDialog
-          publication={ activeRow }
-        />
+        {
+          activeRow && (
+            <Dialog onClose={ handleCloseDialog } open={ dialogOpen }>
+              <DialogTitle>{ activeRow.title }</DialogTitle>
+              <Divider />
+              <Stack
+                direction="row"
+                divider={ <Divider orientation="vertical" flexItem /> }
+                sx={{ px: 3 }}
+              >
+                <Stack direction="row" alignItems="center" gap={ 1 } sx={{ p: 1 }}>
+                  <NihLogo />
+                  <Typography>Abstract: { activeRow.pmid && (
+                    <Link to={ activeRow.pubmed_url }>
+                      { activeRow.pmid }
+                    </Link>) }
+                  </Typography>
+                </Stack>
+                <Stack direction="row" alignItems="center" gap={ 1 } sx={{ p: 1 }}>
+                  <NihLogo />
+                  <Typography>Full text: { activeRow.pmcid && (
+                    <Link to={ activeRow.pmc_url }>
+                      { activeRow.pmcid }
+                    </Link>) }
+                  </Typography>
+                </Stack>
+              </Stack>
+              <Divider />
+              <DialogContent>
+                <Typography paragraph>
+                  <strong>Snippet:</strong> <em>{ activeRow.snippet }</em>
+                </Typography>
+              </DialogContent>
+            </Dialog>
+          )
+        }
       </Card>
     </Fade>
   )
