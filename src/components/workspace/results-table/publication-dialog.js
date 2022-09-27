f9a3b5903cd83fb9babb9884bd2bc138386eb3c4
Matt Watson <mwatson@renci.org>
2022-09-27 16:30:20 -0400

    polish styling dialog

--- src/components/workspace/results-table/publication-dialog.js
+++ src/components/workspace/results-table/publication-dialog.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import PropTypes from 'prop-types'
 import {
   Dialog, Divider, DialogContent, DialogTitle, Stack, Typography,
@@ -12,37 +13,44 @@ const LittleNihLogo  = () => <img src={ nihLogoIcon } height="12" />
 //
 
 export const PublicationDialog = ({ onClose, open, publication }) => {
+
+  const PubmedLink = useCallback(() => {
+    return publication.pmid && publication.pubmed_url ? (
+      <Stack direction="row" alignItems="center" gap={ 1 } sx={{ p: 1 }}>
+        <LittleNihLogo />
+        <Link to={ publication.pubmed_url }>Abstract</Link>
+      </Stack>
+    ) : null
+  }, [publication])
+
+  const PubmedCentralLink = useCallback(() => {
+    return publication.pmcid && publication.pmc_url ? (
+      <Stack direction="row" alignItems="center" gap={ 1 } sx={{ p: 1 }}>
+        <LittleNihLogo />
+        <Link to={ publication.pmc_url }>Full Text</Link>
+      </Stack>
+    ) : null
+  }, [publication])
+
   return (
     <Dialog onClose={ onClose } open={ open }>
       <DialogTitle>{ publication.title }</DialogTitle>
+
       <Divider />
-      <Stack
-        direction="row"
-        divider={ <Divider orientation="vertical" flexItem /> }
-        sx={{ px: 3 }}
-      >
-        {
-          publication.pmid && publication.pubmed_url && (
-            <Stack direction="row" alignItems="center" gap={ 1 } sx={{ p: 1 }}>
-              <LittleNihLogo />
-              <Link to={ publication.pubmed_url }>
-                Abstract
-              </Link>
-            </Stack>
-          )
-        }
-        {
-          publication.pmcid && publication.pmc_url && (
-            <Stack direction="row" alignItems="center" gap={ 1 } sx={{ p: 1 }}>
-              <LittleNihLogo />
-              <Link to={ publication.pmc_url }>
-                Full Text
-              </Link>
-            </Stack>
-          )
-        }
-      </Stack>
+      
+      <DialogContent sx={{ py: 0, backgroundColor: '#e6e9ec' }}>
+        <Stack
+          direction="row" alignItems="center"
+          divider={ <Divider orientation="vertical" flexItem /> }
+        >
+          <Typography sx={{ paddingRight: 1 }}>Links:</Typography>
+          <PubmedLink />
+          <PubmedCentralLink />
+        </Stack>
+      </DialogContent>
+      
       <Divider />
+      
       <DialogContent sx={{ minHeight: '300px' }}>
         <Typography paragraph>
           <strong>Snippet:</strong> <em>{ publication.snippet }</em>
