3c43aa0e40eac61c0ff6247622d184e2a8bd82c6
Matt Watson <mwatson@renci.org>
2022-12-09 11:20:30 -0500

    account for maximal terms in ontology test
    remove debug logging

--- src/util/owl.test.js
+++ src/util/owl.test.js
@@ -18,7 +18,7 @@ test('term extraction from owl file works', () => {
     expect(terms.length).toBeGreaterThan(0);
 
     /*
-      this test verifies that each term is shaped like the following.
+      this test verifies that each term is shaped like this example:
       {
         id: 'CatatonicSchizophrenia',
         labels: [ 'CatatonicSchizophrenia', 'Schizophrenia, catatonic' ],
@@ -29,9 +29,9 @@ test('term extraction from owl file works', () => {
       const termKeys = Object.keys(term)
       return (
         JSON.stringify(termKeys.sort()) === '["id","labels","parentId"]'
-        && typeof term.id === 'string'
-        && Array.isArray(term.labels)
-        && typeof term.parentId === 'string'
+          && typeof term.id === 'string'
+          && Array.isArray(term.labels)
+          && (typeof term.parentId === 'string' || term.parentId === null)
       )
     })
     expect(allShapedRight).toBe(true)
