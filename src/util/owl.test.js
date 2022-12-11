1edf4c8c1838adecf0fff26d357c3752b49d77c4
Matt Watson <mwatson@renci.org>
2022-12-11 13:11:05 -0500

    adjust test to accommodate for the newly-pulled-in seeAlso field

--- src/util/owl.test.js
+++ src/util/owl.test.js
@@ -28,7 +28,7 @@ test('term extraction from owl file works', () => {
     const allShapedRight = terms.every(term => {
       const termKeys = Object.keys(term)
       return (
-        JSON.stringify(termKeys.sort()) === '["id","labels","parentId"]'
+        JSON.stringify(termKeys.sort()) === '["id","labels","parentId","seeAlso"]'
           && typeof term.id === 'string'
           && Array.isArray(term.labels)
           && (typeof term.parentId === 'string' || term.parentId === null)
