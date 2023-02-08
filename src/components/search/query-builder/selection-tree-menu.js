70d41e250a40ab3e8fc6b28f253cb0194e4e5be7
David Glymph <Woozl@users.noreply.github.com>
2023-02-08 10:32:34 -0500

    make action buttons colored on hover

--- src/components/search/query-builder/selection-tree-menu.js
+++ src/components/search/query-builder/selection-tree-menu.js
@@ -1,17 +1,31 @@
 import PropTypes from 'prop-types'
 import {
-  Box, IconButton, Tooltip,
+  Box, IconButton, Tooltip, useTheme,
 } from '@mui/material'
 
-//
-
 export const SelectionTreeMenu = ({ items }) => {
+  const theme = useTheme();
+
   return (
     <Box ml='auto' sx={{ display: 'flex'}}>
       {
-        items.map(({ key, action, icon, tooltip }) => (
+        items.map(({ key, action, icon, tooltip, color }) => (
           <Tooltip key={`term-action-${key}`} title={tooltip} placement="top">
-            <IconButton onClick={action} size="small">{icon}</IconButton>
+            <IconButton
+              onClick={action}
+              size="small"
+              sx={{
+                '--delay': '250ms',
+                color: theme.palette.primary,
+                opacity: 0.4,
+                transition: 'color var(--delay), opacity var(--delay)',
+                '&:hover': { 
+                  color, 
+                  opacity: 1,
+                  transition: 'color var(--delay), opacity var(--delay)',
+                }}}>
+              {icon}
+              </IconButton>
           </Tooltip>
         ))
       }
@@ -25,6 +39,7 @@ SelectionTreeMenu.propTypes = {
       action: PropTypes.func.isRequired,
       icon: PropTypes.node.isRequired,
       tooltip: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired
     })
   ).isRequired,
 }
