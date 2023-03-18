77f27ff4d87343e409c909c0e3f48e6606640c8f
David Glymph <Woozl@users.noreply.github.com>
2023-03-17 21:23:10 -0400

    add popover and icon button

---
+++ src/components/status-indicator.js
@@ -0,0 +1,74 @@
+import { Circle } from "@mui/icons-material";
+import { IconButton, Paper, Popover, Tooltip, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { useState } from "react";
+
+const PopoverPaper = styled(Paper)(({theme}) => ({
+  padding: theme.spacing(2),
+  filter: `drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.3))`,
+  backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.16), rgba(255, 255, 255, 0.16))',
+
+  position: 'relative',
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    top: '-15px',
+    left: '0',
+    right: '0',
+    margin: '0 auto',
+    clipPath: "path('M 0 15 L 10 0 L 20 15 Z')",
+    width: '20px',
+    height: '15px',
+    backgroundColor: theme.palette.background.paper,
+    backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.16), rgba(255, 255, 255, 0.16))',
+  }
+}));
+
+export const StatusIndicator = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  return (
+    <>
+      <Tooltip placement="bottom" title="All services are operational">
+        <IconButton color="success" onClick={handleClick}>
+          <Circle fontSize="xs" />
+        </IconButton>
+      </Tooltip>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        sx={{ marginTop: '15px',
+        '& .MuiPopover-paper': {overflowY: 'visible', overflowX: 'visible'} }}
+        PaperProps={{ style: {boxShadow: 'none'}}}
+      >
+        <PopoverPaper>
+          <Typography>Service 1: Operational</Typography>
+          <Typography>Service 2: Operational</Typography>
+          <Typography>Service 3: Operational</Typography>
+        </PopoverPaper>
+      </Popover>
+    </>
+  );
+};
