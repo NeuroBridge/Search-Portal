f41d6292acb661fb77ee5ad0bab516edb37883c9
Matt Watson <mwatson@renci.org>
2023-03-29 13:51:16 -0400

    chunk docs page content

--- src/views/documentation.js
+++ src/views/documentation.js
@@ -7,61 +7,72 @@ import { ResponsiveIframe } from '../components/responsive-iframe'
 export const DocumentationView = () => {
   return (
     <Container maxWidth="md">
-      <Typography variant="h4" component="h1">Documentation</Typography>
+      <Typography variant="h4" component="h1">Using the Search Portal</Typography>
 
       <br />
 
-      <Typography variant="h5" component="h2">
-        Using the Search Portal
-      </Typography>
-
       <Typography paragraph>
-        Results are retrieved from two sources: PubMed Central, via our API,{' '}
-        <Link to="https://neuroquery.org/">NeuroQuery</Link>.
-        Results are grouped by their source and contain links to the
-        corresponding articles.
+        This interface allows users to construct a query with concepts in the NeuroBridge ontology.
+        The query mechanism makes use of the ontological relationships to find publications in
+        two sources: <Link to="https://www.ncbi.nlm.nih.gov/pmc/">PubMed Central</Link>, via our API,
+        and <Link to="https://neuroquery.org/">NeuroQuery</Link>.
       </Typography>
 
       <Typography variant="h5" component="h2">
         Building a Query
       </Typography>
 
-      <Typography paragraph>
-        The goal of this tool is to build a query from concepts in the NeuroBridge ontology. 
+      <Typography variant="h6" component="h2">
+        Adding Concepts
       </Typography>
 
       <Typography paragraph>
-        The first step to building a query is to add concepts to the query builder with
-        the &ldquo;+ Add Concept&rdquo; button.
+        The first step to building a query is to add concepts to the query builder with the &ldquo;Add Concept&rdquo; button.
         Each concept defines an induced subtree rooted at that concept.
-        Adding a concept to the query builder makes its descendant concepts available for use in the query construction.
+        Adding a concept to the query builder makes its descendant concepts available to the query builder.
       </Typography>
 
       <ResponsiveIframe
         src="https://drive.google.com/file/d/1FbZPj1L_NbJ57KsIfkgIDlAB9N-Md1UN/preview"
       />
 
-      <Typography variant="h5" component="h2">
-        Configuration Options & Viewing the Raw Query
+      <Typography variant="h6" component="h2">
+        Toggling Concepts
       </Typography>
 
       <Typography paragraph>
-        Each concept in the query builder can have one of two states&mdash;either it is in the query or not.
+        Each concept in the query builder can have one of two states&mdash;either it is in the query, or it is not.
         Clicking a concept toggles between the two states, and an icon indicates the concept&apos;s present state.
-        Additionally, changing the state of a concept also changes the states of its descendant concepts to match.
+        Additionally, toggling the state of a concept also toggles the states of its descendant concepts to match the clicked concept.
+      </Typography>
+
+      <ResponsiveIframe />
+
+      <Typography variant="h6" component="h2">
+        Configuration & the Raw Query
+      </Typography>
+
+      <Typography paragraph>
+        Use the &ldquo;View/Hide Query&rdquo; button to toggle visibility of the raw query being constructed.
+        This lets you visually inspect the query before sending the request.
+        By default, the boolean operator between each concept tree is <code>AND</code>,
+        and the <code>OR</code> operator is used within each concept tree,
+        but this can be changed in the Options window that opens upon clicking the &ldquo;Options&rdquo; button.
+      </Typography>
+
+      <ResponsiveIframe />
+
+      <Typography variant="h6" component="h2">
+        Starting Over
+      </Typography>
+
+      <Typography paragraph>
+        Use the &ldquo;RESET&rdquo; button to clear the query builder and start over.
+        Any results already fetched will remain visible.
       </Typography>
 
       <ResponsiveIframe
         src="https://drive.google.com/file/d/1UxC_PioqeFyWYy5-DznT1hLwpWLZ8Wte/preview"
-      />
-
-      <Typography paragraph>
-        Use the &ldquo;View/Hide Query&rdquo; button to toggle visibility of the raw query that will be sent to the NeuroBridge API.
-        The boolean logic within the query can be chaanged in the option modal, which can be opened with the &ldquo;Options&rdquo; button.
-      </Typography>
-  
-      <ResponsiveIframe
-        src="https://drive.google.com/file/d/1dLBjSspmTjzP9rqXdrNu2ZcoOXwWONvr/preview"
       />
 
       <Typography variant="h5" component="h2">
@@ -69,9 +80,17 @@ export const DocumentationView = () => {
       </Typography>
 
       <Typography paragraph>
-        Results are displayed in different tabs based on their source.
+        Results are grouped by their source, ordered by relevance, and contain links to the corresponding articles.
       </Typography>
+  
+      <ResponsiveIframe
+        src="https://drive.google.com/file/d/1dLBjSspmTjzP9rqXdrNu2ZcoOXwWONvr/preview"
+      />
 
+      <Typography paragraph>
+        Use the &ldquo;X&rdquo; in the top-right of the results table to clear the search results.
+      </Typography>
+      
     </Container>
   )
 }
