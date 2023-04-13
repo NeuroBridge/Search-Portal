437c64b770a31d15f9b5b73fac2f16e28a44de07
Matt Watson <mwatson@renci.org>
2023-04-13 12:03:18 -0400

    switch to accordion
    rename video property
    
    style accordion
    
    pass description as prop

--- src/components/tutorial.js
+++ src/components/tutorial.js
@@ -1,105 +1,93 @@
-import { useState } from 'react'
-import { Button, Card, CardContent, Divider, Stack, Stepper, Step, StepButton, Typography } from '@mui/material'
+import { Fragment, useCallback, useMemo, useState } from 'react'
+import PropTypes from 'prop-types'
 import {
-  NavigateBefore as BackIcon,
-  NavigateNext as NextIcon,
-} from '@mui/icons-material'
+  Accordion, AccordionDetails, AccordionSummary,
+  Typography, useTheme,
+} from '@mui/material'
+import { ExpandMore as ExpandIcon } from '@mui/icons-material'
 
-const steps = [
-  {
-    title: 'Adding Concepts',
-    description: `
-      Officia adipisicing aute excepteur fugiat non ut pariatur pariatur in.
-      Consequat excepteur dolor in commodo esse labore exercitation dolor adipisicing eiusmod adipisicing eu aute tempor deserunt cupidatat ut.
-      Sint sunt in qui nisi labore esse culpa eiusmod pariatur.
-      Tempor amet qui quis minim nostrud reprehenderit sint culpa adipisicing commodo.
-    `,
-    videoURL: '',
-  },
-  {
-    title: 'Configuration',
-    description: `
-      Aute ex laboris id ut in eu non dolore voluptate magna ad nulla occaecat veniam.
-      Deserunt ad veniam culpa adipisicing officia nulla duis amet tempor pariatur.
-      Ea quis dolore incididunt laboris dolor sunt in eu.
-      Cupidatat in duis anim aute quis pariatur sit culpa in.
-    `,
-    videoURL: '',
-  },
-  {
-    title: 'Viewing Results',
-    description: `
-      Voluptate excepteur exercitation consequat elit voluptate dolore culpa tempor velit ullamco esse id eu deserunt quis veniam.
-      Aute dolor ut ullamco aute nulla in quis amet.
-      Cillum ut ex qui nostrud cupidatat aliqua nisi magna consequat labore ullamco quis proident id eu est.
-      Quis est commodo magna ea anim officia ut excepteur magna non enim adipisicing est duis.
-    `,
-    videoURL: '',
-  },
-]
+export const Tutorial = ({ title, description, steps }) => {
+  const theme = useTheme()
 
-export const Tutorial = () => {
-  const [activeStep, setActiveStep] = useState(0)
+  /* this varianle holds id of currently-expanded accordion panel */
+  const [expanded, setExpanded] = useState('adding-concepts')
 
-  const isFirstStep = activeStep === 0
-  const isLastStep = activeStep === steps.length - 1
+  const accordionStyle = useMemo(() => ({
+    '.MuiAccordionSummary-content': {
+      display: 'flex',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      gap: theme.spacing(2),
+      '.index': {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.background.paper,
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+        borderRadius: '50%',
+        filter: 'saturate(0.25) opacity(0.5)',
+        transition: 'filter 250ms',
+      },
+      '.title': {
+        color: theme.palette.text.primary,
+        filter: 'opacity(0.75)',
+        transition: 'filter 250ms',
+      },
+    },
+    '&.expanded': {
+      '.index': { filter: 'saturate(1.0) opacity(1.0)' },
+      '.title': { filter: 'opacity(1.0)' },
+    },
+    '&:hover': {
+      '.index': { filter: 'saturate(1.0) opacity(1.0)' },
+      '.title': { filter: 'opacity(1.0)' },
+    },
+  }), [expanded])
 
-  const handleClickNext = () => {
-    setActiveStep(isLastStep ? activeStep : activeStep + 1)
-  }
-
-  const handleClickBack = () => {
-    setActiveStep(isFirstStep ? activeStep : activeStep - 1)
-  }
-
-  const handleClickStep = step => () => {
-    setActiveStep(step)
-  }
+  const handleChange = useCallback(id => (event, isExpanded) => {
+    setExpanded(isExpanded ? id : false)
+  }, [expanded])
 
   return (
-    <Card>
-      <CardContent>
-        <Stepper nonLinear activeStep={ activeStep }>
-          {
-            steps.map((step, i) => (
-              <Step key={ step.title }>
-                <StepButton color="inherit" onClick={ handleClickStep(i) }>
-                  { step.title }
-                </StepButton>
-              </Step>
-            ))
-          }
-        </Stepper>
-      </CardContent>
-
-      <Divider />
-
-      <CardContent>
-        <Typography>
-          { steps[activeStep].description }
-        </Typography>
-      </CardContent>
-
-      <Divider />
-
-      <Stack direction="row" justifyContent="space-between">
-        <Button
-          variant="contained"
-          color="primary"
-          disabled={ isFirstStep }
-          startIcon={ <BackIcon /> }
-          onClick={ handleClickBack }
-        >Back</Button>
-        <Button
-          variant="contained"
-          color="primary"
-          disabled={ isLastStep }
-          endIcon={ <NextIcon /> }
-          onClick={ handleClickNext }
-        >
-          Next
-        </Button>
-      </Stack>
-    </Card>
+    <Fragment>
+      <br />
+      <Typography variant="h5">{ title }</Typography>
+      <Typography paragraph>{ description }</Typography>
+      {
+        steps.map((step, index) => (
+          <Accordion
+            key={ `${ step.id }` }
+            expanded={ expanded === step.id }
+            onChange={ handleChange(step.id) }
+            sx={ accordionStyle }
+            className={ expanded === step.id ? 'expanded' : 'collapsed' }
+          >
+            <AccordionSummary
+              id={ `${ step.id }-header` }
+              aria-controls={ `${ step.id }-content` }
+              expandIcon={ <ExpandIcon /> }
+            >
+              <span className="index">{ index + 1 }</span>
+              <Typography className="title">{ step.title }</Typography>
+            </AccordionSummary>
+            <AccordionDetails className="details">
+              <Typography className="description">{ step.description }</Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))
+      }
+    </Fragment>
   )
+}
+
+Tutorial.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  steps: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  })).isRequired,
 }
--- src/views/documentation.js
+++ src/views/documentation.js
@@ -1,7 +1,84 @@
 import { Container, Typography } from '@mui/material'
 import { Link } from '../components/link'
-import { ResponsiveIframe } from '../components/responsive-iframe'
 import { Tutorial } from '../components/tutorial'
+
+//
+
+const queryBuildingSteps = [
+  {
+    id: 'adding-concepts',
+    title: 'Add Concepts',
+    description: `
+      Officia adipisicing aute excepteur fugiat non ut pariatur pariatur in.
+      Consequat excepteur dolor in commodo esse labore exercitation dolor adipisicing eiusmod adipisicing eu aute tempor deserunt cupidatat ut.
+      Tempor amet qui quis minim nostrud reprehenderit sint culpa adipisicing commodo.
+    `,
+  },
+  {
+    id: 'viewing-query',
+    title: 'View Query',
+    description: `
+      Aute ex laboris id ut in eu non dolore voluptate magna ad nulla occaecat veniam.
+      Ea quis dolore incididunt laboris dolor sunt in eu.
+      Cupidatat in duis anim aute quis pariatur sit culpa in.
+    `,
+  },
+  {
+    id: 'config',
+    title: 'Configuration Options',
+    description: `
+      Voluptate excepteur exercitation consequat elit voluptate dolore culpa tempor velit ullamco esse id eu deserunt quis veniam.
+      Quis est commodo magna ea anim officia ut excepteur magna non enim adipisicing est duis.
+    `,
+  },
+  {
+    id: 'raw-query',
+    title: 'View the Raw Query',
+    description: `
+      Adipisicing in enim dolor ut incididunt amet dolore elit culpa veniam incididunt velit incididunt consectetur.
+      Sunt anim nostrud sed eu aliquip sed ut ullamco reprehenderit.
+    `,
+  },
+  {
+    id: 'remove-concepts',
+    title: 'Remove Concepts',
+    description: `
+      Est irure fugiat non deserunt cupidatat eu elit reprehenderit amet laboris cupidatat.
+      In pariatur ut ut dolore culpa sit exercitation commodo ea.
+    `,
+  },
+  {
+    id: 'submit',
+    title: 'Submit Query',
+    description: `
+      Lorem ipsum sit in occaecat commodo amet nulla ullamco ut ut ut ullamco in ex velit cillum elit.
+      Lorem ipsum est ut ullamco culpa labore nisi exercitation velit sit ea eiusmod in aute do culpa et.
+    `,
+  },
+]
+
+const resultsPanelSteps = [
+  {
+    id: 'overview',
+    title: 'Overview',
+    description: `
+      Labore cillum ad laboris adipisicing deserunt dolor in labore deserunt enim.
+      Cillum et ex in ullamco in deserunt ea cupidatat adipisicing.
+    `,
+  },
+]
+
+const ontologyBrowserSteps = [
+  {
+    id: 'overview',
+    title: 'Overview',
+    description: `
+      Lorem ipsum duis nulla proident ex adipisicing ut esse enim incididunt excepteur.
+      Dolor culpa occaecat aute commodo laborum cillum fugiat enim ullamco.
+      Lorem ipsum ullamco in culpa eu laborum sunt pariatur sint sint amet ex veniam ex.
+    `,
+  },
+]
 
 //
 
@@ -19,83 +96,30 @@ export const DocumentationView = () => {
         and <Link to="https://neuroquery.org/">NeuroQuery</Link>.
       </Typography>
 
-      <Tutorial />
+      <Tutorial
+        title="Query Builder"
+        description="Lorem ipsum tempor excepteur eu occaecat occaecat aute sed est quis."
+        steps={ queryBuildingSteps }
+      />
 
       <br />
 
-      <Typography variant="h5" component="h2">
-        Building a Query
-      </Typography>
-
-      <Typography variant="h6" component="h2">
-        Adding Concepts
-      </Typography>
-
-      <Typography paragraph>
-        The first step to building a query is to add concepts to the query builder with the &ldquo;Add Concept&rdquo; button.
-        Each concept defines an induced subtree rooted at that concept.
-        Adding a concept to the query builder makes its descendant concepts available to the query builder.
-      </Typography>
-
-      <ResponsiveIframe
-        src="https://drive.google.com/file/d/1FbZPj1L_NbJ57KsIfkgIDlAB9N-Md1UN/preview"
+      <Tutorial
+        title="Results Panel"
+        description="Irure in labore quis enim velit adipisicing amet excepteur et."
+        steps={ resultsPanelSteps }
       />
 
-      <Typography variant="h6" component="h2">
-        Toggling Concepts
-      </Typography>
+      <br />
 
-      <Typography paragraph>
-        Each concept in the query builder can have one of two states&mdash;either it is in the query, or it is not.
-        Clicking a concept toggles between the two states, and an icon indicates the concept&apos;s present state.
-        Additionally, toggling the state of a concept also toggles the states of its descendant concepts to match the clicked concept.
-      </Typography>
-
-      <ResponsiveIframe />
-
-      <Typography variant="h6" component="h2">
-        Configuration & the Raw Query
-      </Typography>
-
-      <Typography paragraph>
-        Use the &ldquo;View/Hide Query&rdquo; button to toggle visibility of the raw query being constructed.
-        This lets you visually inspect the query before sending the request.
-        By default, the boolean operator between each concept tree is <code>AND</code>,
-        and the <code>OR</code> operator is used within each concept tree,
-        but this can be changed in the Options window that opens upon clicking the &ldquo;Options&rdquo; button.
-      </Typography>
-
-      <ResponsiveIframe />
-
-      <Typography variant="h6" component="h2">
-        Starting Over
-      </Typography>
-
-      <Typography paragraph>
-        Use the &ldquo;RESET&rdquo; button to clear the query builder and start over.
-        Any results already fetched will remain visible.
-      </Typography>
-
-      <ResponsiveIframe
-        src="https://drive.google.com/file/d/1UxC_PioqeFyWYy5-DznT1hLwpWLZ8Wte/preview"
+      <Tutorial
+        title="Ontology Browser"
+        description="Reprehenderit veniam sed elit officia irure ut deserunt reprehenderit."
+        steps={ ontologyBrowserSteps }
       />
 
-      <Typography variant="h5" component="h2">
-        Results
-      </Typography>
+      <br />
 
-      <Typography paragraph>
-        Results are grouped by their source, ordered by relevance, and contain links to the corresponding articles.
-      </Typography>
-  
-      <ResponsiveIframe
-        src="https://drive.google.com/file/d/1dLBjSspmTjzP9rqXdrNu2ZcoOXwWONvr/preview"
-      />
-
-      <Typography paragraph>
-        Use the &ldquo;X&rdquo; in the top-right of the results table to clear the search results.
-      </Typography>
-      
     </Container>
   )
 }
